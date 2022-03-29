953b6f6e28df3e48360120dadbb80cf3f5cbaa20
Darren Harris <dazza@me.com>
2022-03-29 20:40:37 +0800

    Initial commit

---
+++ test/cdk-vertest.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkVertest from '../lib/cdk-vertest-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-vertest-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkVertest.CdkVertestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
